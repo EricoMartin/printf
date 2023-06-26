944adc900c30fc23826cf8239cbb3ded32760acf
fae713 <favourmichael47@gmail.com>
2023-06-26 12:31:37 -0700

    task 1

--- main.h
+++ main.h
@@ -1,10 +1,10 @@
 #ifndef PRINT_F
 #define PRINT_F
 
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdint.h>
 
 /**
 * struct convert - defines a structure for symbols and functions
@@ -26,7 +26,7 @@ int parser(const char *format, converter funct_list[], va_list args);
 int print_char(va_list);
 int print_string(va_list args);
 int print_percent(va_list);
-int print_num(va_list args);
-int print_i(va_list args);
+int print_num(va_list);
+int print_i(va_list);
 
 #endif
