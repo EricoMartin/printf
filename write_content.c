9f65fd9a47b4faa64e44266695ac67a3b5da57d3
ericomartin <ericomartin.0785@gmail.com>
2023-06-27 07:27:03 -0700

    print binary

--- write_content.c
+++ write_content.c
@@ -69,7 +69,7 @@ int print_num(va_list args)
 
 	{
 		count = count + _putchar('-');
-                n = numb * -1; /* This will evaluate to zero*/
+		n = numb * -1; /* This will evaluate to zero*/
 	}
 	else
 	{
@@ -102,92 +102,3 @@ int print_i(va_list args)
 	return (n);
 }
 
-/**
- * print_unsgined_number - Prints an unsigned number
- * @n: unsigned integer to be printed
- * Return: The amount of numbers printed
- */
-int print_unsgined_number(unsigned int n)
-{
-	int div;
-	int len;
-	unsigned int num;
-
-	div = 1;
-	len = 0;
-
-	num = n;
-
-	for (; num / div > 9; )
-		div *= 10;
-
-	for (; div != 0; )
-	{
-		len += _write_char('0' + num / div);
-		num %= div;
-		div /= 10;
-	}
-
-	return (len);
-}
-
-/**
- * unsigned_integer - Prints Unsigned integers
- * @list: List of all of the argumets
- * Return: a count of the numbers
- */
-int unsigned_integer(va_list list)
-{
-	unsigned int num;
-
-	num = va_arg(list, unsigned int);
-
-	if (num == 0)
-		return (print_unsgined_number(num));
-
-	if (num < 1)
-		return (-1);
-	return (print_unsgined_number(num));
-}
-
-/**
- * print_binary - Converts a number from base 10 to binary
- * @list: List of arguments passed to this function
- * Return: The length of the number printed
- */
-int print_binary(va_list list)
-{
-	unsigned int num;
-	int i, len;
-	char *str;
-	char *rev_str;
-
-	num = va_arg(list, unsigned int);
-	if (num == 0)
-		return (_write_char('0'));
-	if (num < 1)
-		return (-1);
-	len = base_len(num, 2);
-	str = malloc(sizeof(char) * len + 1);
-	if (str == NULL)
-		return (-1);
-
-	i = 0;
-	while (num > 0)
-	{
-		if (num % 2 == 0)
-			str[i] = '0';
-		else
-			str[i] = '1';
-		num = num / 2;
-	i++;
-	}
-	str[i] = '\0';
-	rev_str = rev_string(str);
-	if (rev_str == NULL)
-		return (-1);
-	write_base(rev_str);
-	free(str);
-	free(rev_str);
-	return (len);
-}
